1a59f299438bb60345d68c0d3fbdb43403c927b6
binhtv1998 <61540127+binhtv1998@users.noreply.github.com>
2021-11-02 20:34:23 +0700

    update layout

---
+++ server.js
@@ -0,0 +1,46 @@
+const cors = require('cors');
+let express = require('express');
+//setup express app 
+const request = require('request');
+const session = require('express-session');
+let app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+let http = require('https');
+const productRouter = require('./router.product');
+
+
+var whitelist = ['http://localhost:4200', 'http://localhost'];
+var corsOptions = {
+    origin: function (origin, callback) {
+        // if (!origin || whitelist.indexOf(origin) !== -1) {
+        //     callback(null, true)
+        // } else {
+        //     callback(new Error('Not allowed by CORS'))
+        // }
+        callback(null, true)
+    }
+}
+
+app.use(cors(corsOptions));
+//basic route for homepage
+app.get('/', (req, res) => {
+    res.json('welcome to express app');
+});
+
+app.use('/api/product', productRouter);
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+//     () => { console.log('Database is connected'); },
+//     err => { console.log('Cannot connect to the database' + err) }
+// );
+
+const port = process.env.PORT || 4040;
+const server = app.listen(port, (err) => {
+    if (err)
+        throw err;
+    console.log('listening on port 4200');
+});
+
